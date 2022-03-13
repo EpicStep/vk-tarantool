70aab9b709ded06a66d8f4e5b9b553a4782d6e08
K.Gvencadze <k.gvencadze@icloud.com>
2022-03-14 01:11:54 +0300

    feat(internal/analytics): add error comparing via errors.Is() + add correct response headers on errors e.g. 404 if link doesn't exists

--- internal/shorter/analytics.go
+++ internal/shorter/analytics.go
@@ -1,17 +1,32 @@
 package shorter
 
 import (
+	"errors"
 	"github.com/EpicStep/vk-tarantool/internal/jsonutil"
+	"github.com/EpicStep/vk-tarantool/internal/shorter/database"
 	v1 "github.com/EpicStep/vk-tarantool/pkg/api/v1"
 	ua "github.com/mileusna/useragent"
+	"log"
 	"net/http"
 )
 
 func (s *Service) Analytics(w http.ResponseWriter, r *http.Request) {
 	hash := r.URL.Query().Get("hash")
 
+	if hash == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 	t, err := s.db.GetTransitionByShort(hash)
 	if err != nil {
+		if errors.Is(err, database.ErrNotFound) {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
