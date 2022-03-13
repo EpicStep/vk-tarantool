63cc50650b8b07d0bcf87bf9302aa578f1f994e3
K.Gvencadze <k.gvencadze@icloud.com>
2022-03-14 01:07:56 +0300

    feat(internal/shorter): add writing header in errors e.g. 400 if user input is incorrect

--- internal/shorter/shorter.go
+++ internal/shorter/shorter.go
@@ -4,28 +4,38 @@ import (
 	"crypto/md5"
 	"encoding/base64"
 	"encoding/hex"
-	"fmt"
+	"errors"
+	"github.com/EpicStep/vk-tarantool/internal/jsonutil"
 	shortDB "github.com/EpicStep/vk-tarantool/internal/shorter/database"
 	"github.com/EpicStep/vk-tarantool/internal/shorter/model"
+	v1 "github.com/EpicStep/vk-tarantool/pkg/api/v1"
 	"github.com/EpicStep/vk-tarantool/pkg/database"
+	"github.com/EpicStep/vk-tarantool/pkg/metrics"
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
+	"github.com/prometheus/client_golang/prometheus"
+	"log"
 	"net/http"
 	"strings"
 )
 
 type Service struct {
-	db *shortDB.ShorterDB
+	db      *shortDB.ShorterDB
+	metrics *metrics.PrometheusService
 }
 
 func New(db *database.DB) *Service {
-	return &Service{db: shortDB.New(db)}
+	return &Service{
+		db:      shortDB.New(db),
+		metrics: metrics.NewPrometheusService(),
+	}
 }
 
 func (s *Service) Short(w http.ResponseWriter, r *http.Request) {
 	url := r.URL.Query().Get("url")
 
 	if url == "" {
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
@@ -39,38 +49,61 @@ func (s *Service) Short(w http.ResponseWriter, r *http.Request) {
 	ipArr := strings.Split(r.RemoteAddr, ":")
 
 	err := s.db.CreateShort(&model.Short{
-		Shorted:  ub64[0:12],
-		Original: url,
+		Shorted:   ub64[0:12],
+		Original:  url,
 		CreatedBy: ipArr[0],
 	})
 
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
-	w.Write([]byte("http://37.139.34.190/" + ub64[0:12]))
+	s.metrics.TotalRequests.With(prometheus.Labels{
+		"method": r.Method,
+		"path":   r.RequestURI,
+	}).Inc()
+
+	jsonutil.MarshalResponse(w, http.StatusOK, v1.ShorterResponse{
+		Shorted: "http://localhost:8182/" + ub64[0:12],
+	})
 }
 
 func (s *Service) Transition(w http.ResponseWriter, r *http.Request) {
 	hash := chi.URLParam(r, "hash")
 
-	short, err := s.db.GetShortByHash(hash)
-	if err != nil {
+	if hash == "" {
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	if len(short) <= 0 {
+	short, err := s.db.GetShortByHash(hash)
+	if err != nil {
+		if errors.Is(err, shortDB.ErrNotFound) {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	ipArr := strings.Split(r.RemoteAddr, ":")
 
-	_ = s.db.InsertAnalytics(&model.Transition{
-		ID: uuid.New().String(),
+	err = s.db.InsertAnalytics(&model.Transition{
+		ID:      uuid.New().String(),
 		Shorted: hash,
 		IP:      ipArr[0],
 		UA:      r.UserAgent(),
 	})
+
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
 	http.Redirect(w, r, short[0].Original, http.StatusMovedPermanently)
 }
